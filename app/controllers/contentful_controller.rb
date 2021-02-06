a0dbec32ac672292092a3f7184c47f5d5826ace1
Ikenna Omekam <iomekam@github.com>
2021-02-06 17:17:29 -0500

    add controller for webhook:

---
+++ app/controllers/contentful_controller.rb
@@ -0,0 +1,20 @@
+class ContentfulController < ApplicationController
+  def execute
+    body = JSON.parse(request.body.read)
+
+    case request.headers["X-Contentful-Topic"]
+    when "ContentManagement.Entry.publish"
+      publish(body)
+    end
+
+    render :ok
+  end
+
+  private
+
+  def publish(body)
+    contentful_id = body["sys"]["id"]
+    content_type = body["sys"]["contentType"]["sys"]["id"]
+    ProductSyncJob.perform_later(contentful_id: contentful_id, content_type: content_type)
+  end
+end
---
+++ spec/controllers/contentful_controller_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe ContentfulController, :type => :request do
+  let(:contentful_id) { Faker::Alphanumeric.alpha }
+  let(:content_type) { Faker::Alphanumeric.alpha }
+
+  before(:each) do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
+  it "should queue product sync job when new content published" do
+    headers = { "X-Contentful-Topic" => "ContentManagement.Entry.publish" }
+    payload = {
+      :sys => {
+        :id => contentful_id,
+        :contentType => {
+          :sys => {
+            :id => content_type
+          }
+        }
+      }
+    }
+
+    post "/", :params => payload.to_json, :headers => headers
+
+    expect(ProductSyncJob).to have_been_enqueued.with({contentful_id: contentful_id, content_type: content_type })
+  end
+end
