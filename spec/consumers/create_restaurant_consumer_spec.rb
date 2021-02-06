03c4f9f04ba521a22010f031738226eddd3784d8
Ikenna Omekam <iomekam@github.com>
2021-02-06 15:05:14 -0500

    add cosumer test

---
+++ spec/consumers/create_restaurant_consumer_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe CreateRestaurantConsumer do
+  let(:contentful_product) { create(:product, id: "id") }
+  let(:message) {
+    {
+      ecommerce_id: Faker::Alphanumeric.alpha,
+      name: Faker::Alphanumeric.alpha
+    }
+  }
+  subject(:consumer) { described_class.new }
+
+  before(:each) do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
+  it "should enqueue create restaurant jobs" do
+    consumer.process(message)
+    expect(CreateRestaurantJob).to have_been_enqueued.with(hash_including({
+      ecommerce_id: message[:ecommerce_id],
+      name: message[:name],
+    }))
+  end
+end
