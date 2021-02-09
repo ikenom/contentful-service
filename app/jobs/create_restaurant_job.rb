96bee0975d9d6e94fb7fa24a6b6eae16909a69aa
Ikenna Omekam <iomekam@github.com>
2021-02-09 00:19:14 -0500

    change comment

--- app/jobs/create_restaurant_job.rb
+++ app/jobs/create_restaurant_job.rb
@@ -4,7 +4,7 @@ class CreateRestaurantJob < ApplicationJob
   queue_as :default
 
   def perform(user_id:, access_token:, space_id:, name:)
-    raise "Restaurant with ecommerce id: #{user_id} already exists" if Restaurant.where(user_id: user_id).exists?
+    raise "Restaurant with user id: #{user_id} already exists" if Restaurant.where(user_id: user_id).exists?
 
     contentful_management_service = ContentfulManagement.new(access_token: access_token)
     restaurant_entry = contentful_management_service.create_restaurant(space_id: space_id, name: name)
